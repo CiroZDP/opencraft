26e4ab4095e20ffa1ad96713da56e726ec469d02
SrFerraaaari <cirod307@gmail.com>
2024-12-22 12:24:11 +0100

    ➕ `main.c`: Add more test code

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
-# define NC_NOSTDLIB 1
-
 # include <nc_stdint.h>
 
 int main()
 {
+	uint32_t example = 2;
+	printf("uint32_t example = %d;\n", example);
+	printf("Okay... it seems like the development is going too slowly.\n");
 	return 0;
 }
