15650b118bc6a6bd3493c54440d537a98becd780
SrFerraaaari <cirod307@gmail.com>
2024-12-29 23:57:35 +0100

    ➕ `nc_stdint.h`: Add full support

--- src/include/ncintern/inttypes.h
+++ src/include/ncintern/inttypes.h
@@ -1,6 +1,8 @@
 # pragma once
 # include "tydetect.h"
+# include "intsizes.h"
 
+// Base integer types
 # ifdef NC_MSC_TYPES
 	typedef signed __int8  int8_t;
 	typedef signed __int16 int16_t;
@@ -55,4 +57,52 @@
 	// Long can change size depending on the architecture
 	typedef unsigned long      uintptr_t;
 
+#endif
+
+/// Additional types
+// Least-width integer types
+typedef int8_t  int_least8_t;
+typedef int16_t int_least16_t;
+typedef int32_t int_least32_t;
+typedef int64_t int_least64_t;
+
+typedef uint8_t  uint_least8_t;
+typedef uint16_t uint_least16_t;
+typedef uint32_t uint_least32_t;
+typedef uint64_t uint_least64_t;
+
+// Fast-width integer types
+# ifdef NC_MSC_TYPES
+	// MSVC typically uses `int` for fast types.
+	typedef int8_t  int_fast8_t;
+	typedef int16_t int_fast16_t;
+	typedef int32_t int_fast32_t;
+	typedef int64_t int_fast64_t;
+
+	typedef uint8_t  uint_fast8_t;
+	typedef uint16_t uint_fast16_t;
+	typedef uint32_t uint_fast32_t;
+	typedef uint64_t uint_fast64_t;
+# elif defined NC_GCC_OLD_TYPES
+	// GCC prefers types that match register size for fast types.
+	typedef int32_t int_fast8_t;
+	typedef int32_t int_fast16_t;
+	typedef int32_t int_fast32_t;
+	typedef int64_t int_fast64_t;
+
+	typedef uint32_t uint_fast8_t;
+	typedef uint32_t uint_fast16_t;
+	typedef uint32_t uint_fast32_t;
+	typedef uint64_t uint_fast64_t;
+# else
+	// Default case for other compilers.
+	typedef int  int_fast8_t;
+	typedef int  int_fast16_t;
+	typedef int  int_fast32_t;
+	typedef int64_t int_fast64_t;
+
+	typedef unsigned int uint_fast8_t;
+	typedef unsigned int uint_fast16_t;
+	typedef unsigned int uint_fast32_t;
+	typedef uint64_t uint_fast64_t;
 #endif
