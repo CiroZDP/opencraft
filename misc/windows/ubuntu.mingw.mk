e6109c86c4c91196d37e1034cad50bc737b5fcb1
SrFerraaaari <cirod307@gmail.com>
2024-12-31 21:16:34 +0100

    🛠️ fix: Wrong order in windows makefile

--- misc/windows/ubuntu.mingw.mk
+++ misc/windows/ubuntu.mingw.mk
@@ -1,15 +1,16 @@
 # Project settings
 PROJ_ROOT  = ../..
 SOURCE_DIR = $(PROJ_ROOT)/src
-SOURCES    = $(SOURCE_DIR)/*.c libglfw3.a
+SOURCES    = $(SOURCE_DIR)/*.c
 INCLUDES   = -I"$(SOURCE_DIR)/include/"
 TARGET_DIR = $(PROJ_ROOT)/build/
 
 # Compiler settings
-CFLAGS = $(INCLUDES) -O2 -Wall -static -Lglfw3 -lopengl32 -lgdi32 -lwinmm
+CFLAGS = $(INCLUDES) -O2 -Wall -static
+LDFLAGS = libglfw3.a -lopengl32 -lgdi32 -lwinmm
 
 x64: $(SOURCE_DIR)/main.c
-	mkdir $(TARGET_DIR) && x86_64-w64-mingw32-gcc $(CFLAGS) -m64 $(SOURCES) -o $(TARGET_DIR)/oc_win64.exe
+	mkdir -p $(TARGET_DIR) && x86_64-w64-mingw32-gcc $(CFLAGS) -m64 $(SOURCES) $(LDFLAGS) -o $(TARGET_DIR)/oc_win64.exe
 
 x32: $(SOURCE_DIR)/main.c
-	mkdir $(TARGET_DIR) && i686-w64-mingw32-gcc   $(CFLAGS) -m32 $(SOURCES) -o $(TARGET_DIR)/oc_win32.exe
+	mkdir -p $(TARGET_DIR) && i686-w64-mingw32-gcc   $(CFLAGS) -m32 $(SOURCES) $(LDFLAGS) -o $(TARGET_DIR)/oc_win32.exe
