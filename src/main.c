a4d437ddb6ba87a6a643460652a53cad6003da3c
SrFerraaaari <cirod307@gmail.com>
2025-01-15 19:02:22 +0100

    ➖  Delete GLFW

--- src/main.c
+++ src/main.c
@@ -1,17 +1,7 @@
 # include <stdio.h>
-# include <stdlib.h>
-# include <GLFW/glfw3.h>
 
 int main(void)
 {
-    if (!glfwInit())
-    {
-        printf("[GLFW]: Unable to initialize GLFW!\n");
-        return EXIT_FAILURE;
-    }
-
-
-    glfwTerminate();
-
-    return EXIT_SUCCESS;
+    printf("Hello world!\n");
+    return 0;
 }
