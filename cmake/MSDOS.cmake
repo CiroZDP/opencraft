76ab65da474918b5d9ca0fa4f9e542c5f73ab20a
SrFerraaaari <cirod307@gmail.com>
2025-02-23 21:33:02 +0100

    🛠️  msdos-ci: Plan A:3

--- cmake/MSDOS.cmake
+++ cmake/MSDOS.cmake
@@ -1,7 +1,7 @@
 message(STATUS "Building for MS-DOS with DJGPP with Debian")
 
 set(CMAKE_SYSTEM_NAME Generic)
-set(CMAKE_C_COMPILER "i586-pc-msdosdjgpp-gcc" CACHE STRING "MS-DOS cross compiler" FORCE)
+set(CMAKE_C_COMPILER gcc)
 set(EXECUTABLE_NAME "oc_msdos")
 set(CMAKE_EXECUTABLE_SUFFIX ".exe")
 
