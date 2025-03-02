1b743bdc911f5446c10bb1c3949d9ad9c00b1c64
SrFerraaaari <cirod307@gmail.com>
2025-03-02 23:41:25 +0100

    ➕ msdos-ci: Attempt to compile (1)

--- cmake/MSDOS.cmake
+++ cmake/MSDOS.cmake
@@ -1,4 +1,7 @@
-message(STATUS "Building for MS-DOS with DJGPP with Debian")
+message(STATUS "Building for MS-DOS with DJGPP")
+
+# Define GLFW root directory first
+set(GLFW_DIR "${CMAKE_SOURCE_DIR}/glfw-unbounded")
 
 # Define GLFW include directory
 include_directories("${GLFW_DIR}/include")
@@ -9,4 +12,4 @@ set(EXECUTABLE_NAME "oc_msdos")
 set(CMAKE_EXECUTABLE_SUFFIX ".exe")
 
 # Build executable
-add_executable(opencraft ${SRC_FILES})
+add_executable(opencraft ${SRC_FILES} ${GLFW_DIR}/glfw.c)
